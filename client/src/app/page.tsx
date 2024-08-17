3531b898a71a6b8371ef9fee8ddd79ef53b62b12
Brandon-Price <brandon.samuel.price@gmail.com>
2024-08-16 22:43:09 -0500

    Add Frontend components, List All Users

--- client/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div>
-      <p>This'll be the Sign In/Login Page</p>
-    </div>
-  );
-}
--- server/index.js
+++ server/index.js
@@ -1,18 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const userRoute = require("./routes/user");
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use("/api/users", userRoute);
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-})
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.options('*', cors({ origin: 'http://localhost:3000' }));
+app.use("/api/users", userRoute);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
